e8e9f05ea527fe44aff732ffac11ba9a62fdae4c
sophiasagan <sophiabraddock82@gmail.com>
2019-11-06 13:53:41 -0500

    working on 2

--- index.js
+++ index.js
@@ -78,6 +78,20 @@ class Person {
 */
 
 class Car {
+  constructor (model, milesPerGallon){
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+
+  fill(gallons) {
+    this.tank += gallons;
+  }
+
+  drive(distance) {
+    
+  }
 
 }
 
